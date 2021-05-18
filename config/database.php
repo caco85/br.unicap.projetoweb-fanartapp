326f76df63283cd031f0edb178f9af2d85c45c57
RenatoOliveira20 <renato.oliveira@unu.com.br>
2021-05-18 01:29:49 -0300

    corrigindo database para pqsql errata

--- config/database.php
+++ config/database.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Str;
-
+$DATABASE_URL=parse_url('DATABASE_URL');
 return [
 
     /*
@@ -66,7 +66,7 @@ return [
         'pgsql' => [
             'driver' => 'pgsql',
             'url' => env('DATABASE_URL'),
-            'host' =>$DATABASE_URL["host"],
+            'host' => $DATABASE_URL["host"],
             'port' => $DATABASE_URL["port"],
             'database' => ltrim($DATABASE_URL["path"],"/"),
             'username' => $DATABASE_URL['user'],
